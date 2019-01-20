5fc6ce757eaa22511b2f4fa17345aac9d1970814
VovaSidorov <VovaCivic@gmail.com>
2019-01-20 16:05:56 +0200

    add style

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+    return(
+        <div className="">
+            <h1>The Rick and Morty API</h1>
+            <h2> Hey, did you ever want to hald a terry fold </h2>
+            <h2> &nbsp;&nbsp;&nbsp; I GOT ONE RIGHT HERE, GRAB MY TERRY FLAP </h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/HeroList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function HeroList() {
+
+    return(
+        <div className="">
+            HeroList
+        </div>
+    );
+};
+
+export default HeroList;
