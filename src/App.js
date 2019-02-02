ebd240b0791c1ece7ed5dee47e79b80b4e7266a8
VovaSidorov <VovaCivic@gmail.com>
2019-02-02 13:04:55 +0200

    removed errors and turn to the implementation of pagination

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, {Component, Fragment} from 'react';
+
 import './App.css';
 import Header from "./components/Header";
+
+import SingleHeroPage from "./components/SingleHeroPage";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import HeroList from "./components/HeroList";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-          <Fragment>
-        <Header title="THE RICK AND MORTY API" subTitle="Hey, did you ever want to hald a terry fold " subTitleHiden="&nbsp;&nbsp;&nbsp; I GOT ONE RIGHT HERE, GRAB MY TERRY FLAP"/>
-        <Route path="/" component={HeroList}/>
-          </Fragment>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Fragment>
+                    <Header
+                        title="The Rick and Morty API"
+                        sub_title="Hey, did you ever want to hold a Terry fold?"
+                        sub_title_hidden="&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;"
+                    />
+                    <Route path="/" exact component={HeroList}/>
+                    <Route path="/personage/:id" component={SingleHeroPage}/>
+                </Fragment>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.css'
 
-const Header = (props) => {
-    const {title,subTitle,subTitleHiden} = props;
+
+function Header(props) {
+    const {title,sub_title,sub_title_hidden} = props;
     return(
-        <section className="Header">
-            <h1 className="Home_Title">{title}</h1>
+        <section className="Header ">
+            <h1 className="Home__Title">{title}</h1>
             <div className="Home__SubTitleWrapper">
-            <h2 className="Home__SubTitle"> {subTitle}</h2>
-            <h2 className="Home__HiddenSubTitle">{subTitleHiden}  </h2>
+                <h2 className="Home__SubTitle">{sub_title}</h2>
+                <h2 className="Home__HiddenSubTitle">{sub_title_hidden}</h2>
             </div>
         </section>
-    );
-};
+    )
+}
 
 export default Header;
